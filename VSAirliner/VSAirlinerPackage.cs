043e0dc23c09607ca59430061009312be1b4e07c
Kevin Peters <kwpeters@gmail.com>
2023-02-04 11:19:44 -0500

    Added example command xyzzy.

--- VSAirliner/VSAirlinerPackage.cs
+++ VSAirliner/VSAirlinerPackage.cs
@@ -25,6 +25,7 @@ namespace VSAirliner
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(VSAirlinerPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class VSAirlinerPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace VSAirliner
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await Xyzzy.InitializeAsync(this);
         }
 
         #endregion
